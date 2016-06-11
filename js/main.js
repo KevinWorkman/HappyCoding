fd867191db7d81a24b11a5c51885bbdd0016fd13
Kevin Workman <kevinaworkman@gmail.com>
2016-06-11 09:48:56 -0400

    initial setup

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-$('#themeChooser').append(new Option("light", "light"));
-$('#themeChooser').append(new Option("dark", "dark"));
+function setupThemeChooser(){
+  $('#themeChooser').append(new Option("light", "light"));
+  $('#themeChooser').append(new Option("dark", "dark"));
+  
+  $('#themeChooser').on('change', function() {
+  
+    var fileName	= $('#themeChooser').val();
+    if("dark" == fileName){
+      $("#bootstrapCss").attr("href", "/css/bootstrap.cyborg.css");
+    }
+    else{
+      $("#bootstrapCss").attr("href", "/css/bootstrap.css");
+    }
+  });
+}
 
-$('#themeChooser').on('change', function() {
-
-  var fileName	= $('#themeChooser').val();
-  if("Default Theme" == fileName){
-    $("#bootstrapCss").attr("href", "css/themes/bootstrap.default.min.css");
-  }
-  else{
-    $("#bootstrapCss").attr("href", "css/themes/bootstrap." + fileName + ".min.css");
-  }
-});
+$(setupThemeChooser);
