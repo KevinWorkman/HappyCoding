d9aeb3d6925adf405dbbab9a48e223dfddba55db
Kevin Workman <kevinaworkman@gmail.com>
2016-06-12 11:11:42 -0400

    initial setup

--- js/main.js
+++ js/main.js
@@ -1,28 +1,29 @@
 function setupThemeChooser(){
-  
-  console.log("setupThemeChooser");
-  
-  $('#themeChooser').append(new Option("dark", "dark"));
-  $('#themeChooser').append(new Option("light", "light"));
-  
-  if(Cookies.get('theme')){
-    console.log("theme: " + Cookies.get('theme'));
-    $('#themeChooser').val(Cookies.get('theme'));
-  }
-  
-  $('#themeChooser').on('change', function() {
-  
-    var selectedTheme	= $('#themeChooser').val();
-    
-    Cookies.set('theme', selectedTheme);
-    
-    if("dark" == selectedTheme){
-      $("#bootstrapCss").attr("href", "/css/bootstrap.cyborg.css");
-    }
-    else{
-      $("#bootstrapCss").attr("href", "/css/bootstrap.css");
-    }
-  });
+	
+	console.log("setupThemeChooser");
+	
+	$('#themeChooser').append(new Option("dark", "dark"));
+	$('#themeChooser').append(new Option("light", "light"));
+	
+	if(Cookies.get('theme')){
+		$('#themeChooser').val(Cookies.get('theme'));
+	}
+	
+	$('#themeChooser').on('change', function() {
+	
+		var selectedTheme	= $('#themeChooser').val();
+		
+		Cookies.set('theme', selectedTheme);
+		
+		if("dark" == selectedTheme){
+			$("#bootstrapCss").attr("href", "/css/bootstrap.cyborg.css");
+			$("#favicon").attr("href", "/css/faviconDark.png");
+		}
+		else{
+			$("#bootstrapCss").attr("href", "/css/bootstrap.css");
+			$("#favicon").attr("href", "/css/favicon.png");
+		}
+	});
 }
 
 $(setupThemeChooser);
