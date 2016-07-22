c26cce9bce4485895503b9ea78d315f1ffa73936
Kevin Workman <kevinaworkman@gmail.com>
2016-07-22 15:29:11 -0400

    navbar

--- js/main.js
+++ js/main.js
@@ -11,33 +11,42 @@ function setupThemeChooser(){
 	
 		var selectedTheme = $('#themeChooser').val();
 		
-		console.log("theme: " + selectedTheme);
-		
 		Cookies.set('theme', selectedTheme);
 		
 		if("dark" == selectedTheme){
-			
 			$(".dark-css").removeAttr('disabled');
 			$(".light-css").attr('disabled', 'disabled');
-			
-		//	$("#bootstrap-css").attr("href", "/css/bootstrap.cyborg.css");
-		//	$("#bootstrap-extension-css").attr("href", "/css/bootstrap-extension-dark.css");
-		//	$("#syntax-css").attr("href", "/css/syntax-dark.css");
 			$("#favicon").attr("href", "/images/faviconDark.png");
+			randomizeNavDark();
 		}
 		else{
-			
 			$(".light-css").removeAttr('disabled');
 			$(".dark-css").attr('disabled', 'disabled');
-			
-		//	$("#bootstrap-css").attr("href", "/css/bootstrap.readable.css");
-		//	$("#bootstrap-extension-css").attr("href", "/css/bootstrap-extension-light.css");
-		//	$("#syntax-css").attr("href", "/css/syntax-light.css");
 			$("#favicon").attr("href", "/images/favicon.png");
+			randomizeNavLight();
 		}
 		
 		setRandomBackground();
 	});
 }
+
+function randomizeNavLight(){
+	var letters = 'ABCDEF'.split('');
+	var color = '#';
+	for (var i = 0; i < 6; i++ ) {
+		color += letters[Math.floor(Math.random() * 6)];
+	}
+	$(".navbar").css('background-color', color);
+}
+
+function randomizeNavDark(){
+	var letters = '0123456789'.split('');
+	var color = '#';
+	for (var i = 0; i < 6; i++ ) {
+		color += letters[Math.floor(Math.random() * 10)];
+	}
+	$(".navbar").css('background-color', color);	
+}
+
 
 $(setupThemeChooser);
