aee237eed7a27536641fd917bfce958dcbcda71a
Kevin Workman <kevinaworkman@gmail.com>
2020-09-07 17:59:13 -0700

    cloud storage example

---
+++ examples/google-cloud/google-cloud-example-projects/cloud-storage-hello-world/README.md
@@ -0,0 +1,9 @@
+This project lets users upload files to Google Cloud Stroage.
+
+You can run this locally by executing this command:
+
+```
+mvn package exec:java
+```
+
+Learn more at [HappyCoding.io/tutorials/google-cloud/cloud-storage](https://happycoding.io/tutorials/google-cloud/cloud-storage).
---
+++ examples/google-cloud/google-cloud-example-projects/cloud-storage-hello-world/src/main/java/io/happycoding/servlets/UploadServlet.java
@@ -0,0 +1,53 @@
+package io.happycoding.servlets;
+
+import com.google.cloud.storage.Blob;
+import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import java.io.IOException;
+import java.io.InputStream;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+@MultipartConfig
+@WebServlet("/upload")
+public class UploadServlet extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    // Submissions can contain other types of data
+    String message = request.getParameter("message");
+
+    // Get the file chosen by the user
+		Part filePart = request.getPart("image");
+    String fileName = filePart.getSubmittedFileName();
+	  InputStream fileInputStream = filePart.getInputStream();
+
+    // Upload the file and get its URL
+    String uploadedFileUrl = uploadToCloudStorage(fileName, fileInputStream);
+
+    // Use the URL to output some HTML
+    response.setContentType("text/html;");
+    response.getWriter().println("<p>Your message: " + message + "</p>");
+    response.getWriter().println("<p>Your image:</p>");
+    response.getWriter().println("<img src=\"" + uploadedFileUrl + "\"  />");
+  }
+  
+  /** Uploads a file to Cloud Storage and returns the uploaded file's URL. */
+  private static String uploadToCloudStorage(String fileName, InputStream fileInputStream) {
+    Storage storage =
+        StorageOptions.newBuilder().setProjectId("happy-coding-gcloud").build().getService();
+    BlobId blobId = BlobId.of("happy-coding-gcloud.appspot.com", fileName);
+    BlobInfo blobInfo = BlobInfo.newBuilder(blobId).build();
+    Blob blob = storage.create(blobInfo, fileInputStream);
+    return blob.getMediaLink();
+  }
+}
