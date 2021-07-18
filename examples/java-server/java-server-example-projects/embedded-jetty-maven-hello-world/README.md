10055aa16d6e6c4e7a735ecd0a00a3d51874f271
Kevin Workman <kevinaworkman@gmail.com>
2021-07-18 14:43:34 -0700

    embedded jetty example

---
+++ examples/java-server/java-server-example-projects/embedded-jetty-maven-hello-world/README.md
@@ -0,0 +1,20 @@
+This project contains a barebones example webapp that uses embedded Jetty and Maven to deploy a server.
+
+This project is laid out like this:
+
+- `embedded-jetty-maven-hello-world/`
+  - **`pom.xml`** is a Maven [POM file](https://maven.apache.org/pom.html) that defines the project.
+  - `src/main/` is a directory that contains the code.
+    - `java/` is a directory that contains server-side code.
+      - `io.happycoding.`**`ServerMain.java`** is the main class that sets up the server.
+      - `io.happycoding.servlets.`**`HelloWorldServlet.java`** is a Java servlet that returns some HTML content.
+    - `webapp/` is a directory that contains web files.
+      - **`index.html`** is an HTML file that shows static content.
+
+You can run this locally by executing this command:
+
+```
+mvn package exec:java
+```
+
+Learn more at [HappyCoding.io/tutorials/java-server](https://happycoding.io/tutorials/java-server).
---
+++ examples/java-server/java-server-example-projects/embedded-jetty-maven-hello-world/src/main/java/io/happycoding/ServerMain.java
@@ -0,0 +1,37 @@
+package io.happycoding;
+
+import java.net.URL;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+/**
+ * Starts up a server that serves static files and annotated servlets.
+ */
+public class ServerMain {
+
+  public static void main(String[] args) throws Exception {
+
+    // Create a server that listens on port 8080.
+    Server server = new Server(8080);
+    WebAppContext webAppContext = new WebAppContext();
+    server.setHandler(webAppContext);
+
+    // Load static content from inside the jar file.
+    URL webAppDir =
+        ServerMain.class.getClassLoader().getResource("META-INF/resources");
+    webAppContext.setResourceBase(webAppDir.toURI().toString());
+
+    // Look for annotations in the classes directory (dev server) and in the
+    // jar file (live server)
+    webAppContext.setAttribute(
+        "org.eclipse.jetty.server.webapp.ContainerIncludeJarPattern", 
+        ".*/target/classes/|.*\\.jar");
+
+    // Start the server! 🚀
+    server.start();
+    System.out.println("Server started!");
+
+    // Keep the main thread alive while the server is running.
+    server.join();
+  }
+}
