ea211b317df667cd45fda99df35324700cd2ba76
Kevin Workman <kevinaworkman@gmail.com>
2021-09-03 18:23:22 -0700

    date.jsp example

---
+++ examples/java-server/java-server-example-projects/jsp-date/src/main/java/io/happycoding/servlets/HelloWorldServlet.java
@@ -0,0 +1,19 @@
+package io.happycoding.servlets;
+
+import java.io.IOException;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/hello")
+public class HelloWorldServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    response.setContentType("text/html;");
+    response.getWriter().println("<h1>Hello world!</h1>");
+  }
+}
---
+++ examples/java-server/java-server-example-projects/jsp-date/src/main/webapp/date.jsp
@@ -0,0 +1,11 @@
+<%@ page import="java.util.Date" %>
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>My Web App</title>
+	</head>
+	<body>
+		<h1>My Web App</h1>
+		<p>The current time is: <%= new Date().toString() %></p>
+	</body>
+</html>
